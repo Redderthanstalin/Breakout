e3978327a7d92d841598994d4c3c84e09dcd1a3a
JustinBrown <justin@cloudheadgames.com>
2018-08-04 08:20:48 -0700

    Committing Freeze Functionality

---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventManager {
+
+    //Fields for the Freezer Event
+    static FreezerBlock freezeEventInvoker;
+    static UnityAction<float> freezeEventListener;
+
+    public static void AddFreezeEventInvoker(FreezerBlock invoker)
+    {
+        freezeEventInvoker = invoker;
+        if(freezeEventListener != null)
+        {
+            freezeEventInvoker.AddFreezeEffectlistener(freezeEventListener);
+        }
+    }
+
+    public static void AddFreezeEventListener(UnityAction<float> listener)
+    {
+        freezeEventListener = listener;
+        if(freezeEventInvoker != null)
+        {
+            freezeEventInvoker.AddFreezeEffectlistener(listener);
+        }
+    }
+}
